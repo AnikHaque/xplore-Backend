77fb4c2a6a38490200c6cf5d55725f0b928ed453
AnikHaque <ekramul15-811@diu.edu.bd>
2023-10-20 04:28:30 +0600

    updated validate request

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject, ZodEffects } from 'zod';
 
+
 export const validateRequest =
   (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
